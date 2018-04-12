84213a9c75388488472553627c2ca92504bf2c76
Shane Weickum <sweick23@outlook.com>
2018-04-12 02:31:56 -0700

    updated design

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const passport = require('passport');
 const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey('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');
+sgMail.setApiKey('MY_API_KEY');
 
 
 
